faa4bdd3fa89251c03a0cc327c269e48e1f75e16
Kieran Emery <kizwiz@hotmail.co.uk>
2023-11-06 01:03:48 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using DependencyInjection.Services;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        // Sets up a default host builder in a .net6 application
+        // Host is now set up with configured services or unconfigured service
+        IHost _host = Host.CreateDefaultBuilder().ConfigureServices(
+            services => {
+                services.AddSingleton < IApplication, Application > ();
+        }).Build();
+
+        // Gets access to dependcy injection & require this service
+        var app = _host.Services.GetRequiredService<IApplication>();
+
+        // Call the method on the item. On the instantiated object.
+        app.Run();
+    }
+}
---
+++ Services/Application.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjection.Services
+{
+    /// <summary>
+    /// Interface.
+    /// </summary>
+    public interface IApplication
+    {
+        void Run();
+    }
+
+    /// <summary>
+    /// Application class.
+    /// </summary>
+    public class Application : IApplication
+    {
+        private readonly ILogger<Application> _logger;
+
+        /// <summary>
+        /// Constructor requires ILogger of Application
+        /// The dependency injetion container created by the default host builder is already rigged to be able to provide this.
+        /// </summary>
+        public Application(ILogger<Application> logger)
+                                        => _logger = logger;
+
+        /// <summary>
+        /// Run method
+        /// </summary>
+        public void Run()
+        {
+            _logger.LogInformation("Logging from the Application class!");
+            Console.WriteLine("Hello, World from the Application class!");
+        }
+    }
+
+}
